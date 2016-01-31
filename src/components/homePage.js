07ad6e5ff6dea933ea8765f7fea143eff74d9db5
Michel-Alexandre Bélanger <michel.alexandre.belanger@gmail.com>
2016-01-30 19:31:34 -0500

    add files

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+
+var React = require('react');
+
+var HomePage = React.createClass({
+	render: function(){
+		return (
+			<div className="jumbotron">
+				<h1>paragraphe</h1>
+				<p>React, react.. component.</p>
+			</div>
+		);
+	}
+
+});
+
+module.exports = HomePage;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var HomePage = require('./components/homePage');
+
+React.render(<HomePage />, document.getElementById('app'));
