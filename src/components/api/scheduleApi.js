1974c7dd2fd932ed757df05537fdb2d99648e948
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-02-17 12:06:34 -0500

    add API

---
+++ src/components/api/scheduleApi.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//This file is mocking a web API by hitting hard coded data.
+var scheduleData = require('./scheduleData').schedule;
+
+var _ = require('lodash');
+
+var _clone = function(item) {
+	//return cloned copy so that the item is passed by value instead of by reference
+	return JSON.parse(JSON.stringify(item)); 
+};
+
+var ScheduleApi = {
+	getSchedule: function() {
+		return _clone(scheduleData); 
+	},
+
+	getHeaders: function(scheduleFormat) {
+		var headers = [];
+		for(var key in scheduleFormat) {
+		    if(scheduleFormat.hasOwnProperty(key)) {
+		        headers.push(key);
+		    }
+		}
+		return headers;
+	},
+
+	getDays: function(scheduleFormat) {
+		var days = [];
+		for(var key in scheduleFormat) {
+		    if(scheduleFormat.hasOwnProperty(key)) {
+		        days.push(scheduleFormat[key]);
+		    }
+		}
+		return days;
+	},
+
+	isDinnerTime: function(scheduleFormat) {
+		var DINNER_TIME = 16;
+		var isDinnerTime = false;
+		var days = this.getDays(scheduleFormat);
+		for (var i in days) {
+		  var courses = days[i];
+		  for (var j in courses) {
+		  	var course = courses[j];
+		  	var endHour = course.endHour.split(":")[0];
+		  	if( endHour > DINNER_TIME ) {
+		  		isDinnerTime = true;
+		  	}
+		  }
+		}
+		return isDinnerTime;
+	},
+};
+
+module.exports = ScheduleApi;
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,97 +1,17 @@
 var React = require('react');
 var Day = require('../day/day');
 
+var ScheduleApi = require('../api/scheduleApi');
+
+
 var Schedule = React.createClass({
 
-	getHeaders: function(scheduleFormat) {
-		var headers = [];
-		for(var key in scheduleFormat) {
-		    if(scheduleFormat.hasOwnProperty(key)) {
-		        headers.push(key);
-		    }
-		}
-		return headers;
-	},
-
-	getDays: function(scheduleFormat) {
-		var days = [];
-		for(var key in scheduleFormat) {
-		    if(scheduleFormat.hasOwnProperty(key)) {
-		        days.push(scheduleFormat[key]);
-		    }
-		}
-		return days;
-	},
-
-
-	isDinnerTime: function(scheduleFormat) {
-		var DINNER_TIME = 16;
-		var isDinnerTime = false;
-		var days = this.getDays(scheduleFormat);
-		for (var i in days) {
-		  var courses = days[i];
-		  for (var j in courses) {
-		  	var course = courses[j];
-		  	var endHour = course.endHour.split(":")[0];
-		  	if( endHour > DINNER_TIME ) {
-		  		isDinnerTime = true;
-		  	}
-		  }
-		}
-		return isDinnerTime;
-	},
-
-	getSchedule: function() {
-        return {
-			lundi : [
-		    	{
-		    		link: "link1",
-		    		image: "http://www.mondeavie.ca/media/course_icon/logo_ki_gong_D2eqw4b.png",
-		    		startHour: "09:00",
-		    		endHour: "11:30",
-		    		professorName: "Danielle Fontaine"
-
-		    	},
-		    	{
-		    		link: "link2",
-		    		image: "http://www.mondeavie.ca/media/course_icon/logo_ki_gong_D2eqw4b.png",
-		    		startHour: "13:00",
-		    		endHour: "15:30",
-		    		professorName: "Alison Martel"
-		    	}
-			],
-			mardi: [
-		    	{
-		    		link: "link1b",
-		    		image: "http://www.mondeavie.ca/media/course_icon/logo_ki_gong_D2eqw4b.png",
-		    		startHour: "09:00",
-		    		endHour: "12:30",
-		    		professorName: "Johanne Lacasse"
-		    	},
-		    	{
-		    		link: "link2b",
-		    		image: "http://www.mondeavie.ca/media/course_icon/logo_ki_gong_D2eqw4b.png",
-		    		startHour: "12:30",
-		    		endHour: "15:00",
-		    		professorName: "Sandra Duval"
-		    	},
-		    	{
-		    		link: "link3b",
-		    		image: "http://www.mondeavie.ca/media/course_icon/logo_ki_gong_D2eqw4b.png",
-		    		startHour: "16:00",
-		    		endHour: "18:30",
-		    		professorName: "Sylvie Major"
-		    	}
-			]
-        };
-	},
-
     getInitialState: function() {
-    	var schedule = this.getSchedule();
-    	console.log(this.isDinnerTime(schedule));
+    	var schedule = ScheduleApi.getSchedule();
+    	console.log(ScheduleApi.isDinnerTime(schedule));
     	return {
-    		headers: this.getHeaders(schedule),
-    		days: this.getDays(schedule)
+    		headers: ScheduleApi.getHeaders(schedule),
+    		days: ScheduleApi.getDays(schedule)
     	};
 
     },
