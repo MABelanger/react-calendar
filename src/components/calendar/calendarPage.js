6b41739ed2a769d4445722105a3a341a65602fb5
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-02-23 23:12:03 -0500

    each course

--- src/components/calendar/calendarPage.js
+++ src/components/calendar/calendarPage.js
@@ -1,10 +1,13 @@
 var React = require('react');
 var Calendar = require('./calendar');
 
+
+
+
 var CalendarPage = React.createClass({
   render: function(){
     return (
-      <div className="container">
+      <div className="container" style={{backgroundColor:"#F5F5F5"}}>
         <Calendar />
       </div>
     );
--- src/components/calendar/courses/courses.js
+++ src/components/calendar/courses/courses.js
@@ -3,19 +3,38 @@ var React = require('react');
 function getCourses() {
 	return (
 		<div className="col-sm-3">
-			<table className="cal-menu ng-scope">
+			<table className="cal-menu">
 				<tr>
 					<td className="cal-menu-cname" colspan="2">
-				    	<a href="" ng-click="teacher_hide = !teacher_hide">
+				    	<a href="" >
 				    		<div className="text-right">
-					    		<div className="cal-menu-cname-label ng-binding">
+					    		<div className="cal-menu-cname-label">
 					    			Yoga
 					    		</div><div className="cal-menu-icon">
-					    			<img ng-src="/media/course_icon/logo_yoga_Zhstq9G.png" src="/media/course_icon/logo_yoga_Zhstq9G.png" />
+					    			<img src="/media/course_icon/logo_yoga_Zhstq9G.png" src="/media/course_icon/logo_yoga_Zhstq9G.png" />
 					    		</div>
 					    	</div>
 				    	</a>
 				    </td>
+				</tr>
+				<tr>
+					<td>
+						<div class="animate-show">
+							<a href="#/courses/2" href="#/courses/2">
+								<div class="text-right cal-menu-tname">
+									Sandra Duval
+								</div>
+							</a>
+						</div>
+						<div class="animate-show">
+							<a href="#/courses/3" href="#/courses/3">
+								<div class="text-right cal-menu-tname">
+									Isabelle Nadeau
+								</div>
+							</a>
+						</div>
+					</td>
+					<td class="cal-menu-empty"></td>
 				</tr>
 			</table>
 		</div>
