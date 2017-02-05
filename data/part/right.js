833d7ed878db5c9a682efeaa683d73700791eb8f
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-02-05 12:50:07 -0500

    cleanup

--- data/part/right.js
+++
@@ -1,6 +0,0 @@
-                <ul>
-                  <li class="coursed-li-day-name"><span class="all-label ">Vendredi</span></li>
-                  <ul>
-                    <li class="coursed-li-hour ">12:00 à 13:15 <a class="link-url" href="#/day_schedules/reserve/18">Réserver</a> <span class=""><strong><u>Complet</u></strong></span><br /> <span class="txt-red"> <span class="ng-scope"> (Gratuit le <span class=" ng-scope">06 mai 2016 )<br /> </span> </span> </span></li>
-                  </ul>
-                </ul>
